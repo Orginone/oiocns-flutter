e67960e4208f5b9e3344d9b45fc150b9d94aa805
jiangzy <296778233@qq.com>
2022-10-10 15:03:47 +0800

    去掉无用的包

--- lib/page/home/organization/units/units_page.dart
+++ lib/page/home/organization/units/units_page.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:orginone/api_resp/target_resp.dart';
-import 'package:orginone/component/bread_crumb.dart';
-import 'package:orginone/component/text_avatar.dart';
 import 'package:orginone/page/home/home_controller.dart';
 import 'package:orginone/page/home/organization/units/units_controller.dart';
 
-import '../../../../api_resp/tree_node.dart';
-import '../../../../component/text_search.dart';
-import '../../../../component/text_tag.dart';
 import '../../../../component/unified_scaffold.dart';
 import '../../../../component/unified_text_style.dart';
-import '../../../../util/string_util.dart';
 import '../../../../util/widget_util.dart';
 
 class UnitsPage extends GetView<UnitsController> {
