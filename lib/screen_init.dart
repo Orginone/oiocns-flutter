abd7c705d7c0853cf73c2e4eecee6313c6e987cd
江泽阳 <296778233@qq.com>
2022-10-13 00:19:46 +0800

    国际化，输入框优化重构

--- lib/screen_init.dart
+++ lib/screen_init.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:orginone/routers.dart';
@@ -15,6 +16,15 @@ class ScreenInit extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return GetMaterialApp(
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate, //iOS
+          ],
+          supportedLocales: const [
+            Locale('zh', 'CN'),
+            Locale('en', 'US'),
+          ],
           textDirection: TextDirection.ltr,
           initialRoute: Routers.main,
           getPages: Routers.getInitRouters(),
