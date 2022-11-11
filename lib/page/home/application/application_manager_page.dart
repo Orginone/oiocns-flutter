30bcf9de40f5b7eddd2ee92338b69cb393e7261f
江泽阳 <296778233@qq.com>
2022-11-12 01:06:47 +0800

    修改

--- lib/page/home/application/application_manager_page.dart
+++ lib/page/home/application/application_manager_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:orginone/component/a_font.dart';
+import 'package:orginone/component/unified_colors.dart';
 import 'package:orginone/component/unified_scaffold.dart';
 import 'package:orginone/util/widget_util.dart';
 
@@ -13,6 +15,17 @@ class ApplicationManagerPage extends GetView<ApplicationManagerController> {
       appBarTitle: Text("资产管理", style: AFont.instance.size22Black3),
       appBarCenterTitle: true,
       appBarLeading: WidgetUtil.defaultBackBtn,
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Container(
+      padding: EdgeInsets.only(left: 25.w, right: 25.w),
+      color: UnifiedColors.navigatorBgColor,
+      child: Column(
+        children: [],
+      ),
     );
   }
 }
