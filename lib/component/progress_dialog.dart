ea30aebbed16017d77541fb0052d6ab24eba2d3d
江泽阳 <296778233@qq.com>
2022-12-07 01:39:44 +0800

    fix: 应用内更新

--- lib/component/progress_dialog.dart
+++ lib/component/progress_dialog.dart
@@ -31,59 +31,64 @@ class UpdaterDialog extends Dialog {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(10.w)),
-          boxShadow: const [
-            //阴影效果
-            BoxShadow(
-              offset: Offset(0, 0), //阴影在X轴和Y轴上的偏移
-              color: Colors.grey, //阴影颜色
-              blurRadius: 3.0, //阴影程度
-              spreadRadius: 0, //阴影扩散的程度 取值可以正数,也可以是负数
-            ),
-          ],
-        ),
-        width: 300.w,
-        height: 400.h,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(padding: EdgeInsets.only(top: 20.h)),
-            image,
-            Padding(padding: EdgeInsets.only(top: 20.h)),
-            description,
-            progressWidget,
-          ],
+      child: Material(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.w)),
+            boxShadow: const [
+              //阴影效果
+              BoxShadow(
+                offset: Offset(0, 0), //阴影在X轴和Y轴上的偏移
+                color: Colors.grey, //阴影颜色
+                blurRadius: 3.0, //阴影程度
+                spreadRadius: 0, //阴影扩散的程度 取值可以正数,也可以是负数
+              ),
+            ],
+          ),
+          width: 300.w,
+          height: 400.h,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(padding: EdgeInsets.only(top: 20.h)),
+              image,
+              Padding(padding: EdgeInsets.only(top: 20.h)),
+              description,
+              progressWidget,
+            ],
+          ),
         ),
       ),
     );
   }
 
-  get progressWidget => Obx(() {
-        if (confirmStatus.startDownloading.value) {
-          switch (confirmStatus.otaStatus.value) {
-            case OtaStatus.DOWNLOADING:
-              return progressBar;
-            case OtaStatus.INSTALLING:
-              return const Text("正在安装中!");
-            case OtaStatus.ALREADY_RUNNING_ERROR:
-              return const Text("下载服务已运行!");
-            case OtaStatus.PERMISSION_NOT_GRANTED_ERROR:
-              return const Text("未授权相关权限!");
-            case OtaStatus.INTERNAL_ERROR:
-              return const Text("工具包服务异常!");
-            case OtaStatus.DOWNLOAD_ERROR:
-              return const Text("apk 包下载异常，请稍后再试!");
-            case OtaStatus.CHECKSUM_ERROR:
-              return const Text("apk 文件校验异常!");
+  get progressWidget => Container(
+        margin: EdgeInsets.only(bottom: 10.h),
+        child: Obx(() {
+          if (confirmStatus.startDownloading.value) {
+            switch (confirmStatus.otaStatus.value) {
+              case OtaStatus.DOWNLOADING:
+                return progressBar;
+              case OtaStatus.INSTALLING:
+                return const Text("正在安装中!");
+              case OtaStatus.ALREADY_RUNNING_ERROR:
+                return const Text("下载服务已运行!");
+              case OtaStatus.PERMISSION_NOT_GRANTED_ERROR:
+                return const Text("未授权相关权限!");
+              case OtaStatus.INTERNAL_ERROR:
+                return const Text("工具包服务异常!");
+              case OtaStatus.DOWNLOAD_ERROR:
+                return const Text("apk 包下载异常，请稍后再试!");
+              case OtaStatus.CHECKSUM_ERROR:
+                return const Text("apk 文件校验异常!");
+            }
+          } else {
+            return confirmBtn;
           }
-        } else {
-          return confirmBtn;
-        }
-      });
+        }),
+      );
 
   get image => Align(
         alignment: Alignment.topCenter,
@@ -114,26 +119,25 @@ class UpdaterDialog extends Dialog {
         ),
       );
 
-  get confirmBtn => Container(
-        margin: EdgeInsets.only(bottom: 10.h),
-        child: ElevatedButton(
-          onPressed: () {
-            _downloadPackage();
-          },
-          style: ButtonStyle(padding: MaterialStateProperty.all(lr40)),
-          child: const Text("立即更新"),
-        ),
+  get confirmBtn => ElevatedButton(
+        onPressed: () {
+          _downloadPackage();
+        },
+        style: ButtonStyle(padding: MaterialStateProperty.all(lr40)),
+        child: const Text("立即更新"),
       );
 
-  get progressBar => GFProgressBar(
+  get progressBar => SizedBox(
+      height: 20.h,
+      width: 180.w,
+      child: GFProgressBar(
         percentage: confirmStatus.progress.value * 1.0 / 100,
-        width: 100.w,
         lineHeight: 20.h,
         radius: 50.w,
         type: GFProgressType.linear,
         backgroundColor: Colors.black26,
         progressBarColor: Colors.lightGreen,
-      );
+      ));
 
   void _downloadPackage() {
     SysUtil.update(prefix, (OtaEvent event) {
