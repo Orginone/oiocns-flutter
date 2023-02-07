1aa7b26c5ddebaa56b96d267d2a18a1bc8f854c9
chenwb <ichenwanbing@163.com>
2023-02-07 11:51:18 +0800

    refactor:market目录ts转dart

---
+++ lib/dart/core/market/market.dart
@@ -0,0 +1,146 @@
+import './model.dart';
+import 'package:orginone/dart/base/model.dart';
+import 'package:orginone/dart/base/schema.dart';
+import 'package:orginone/dart/core/enum.dart';
+
+class Market implements IMarket {
+  late XMarket market;
+
+  late List<TargetType> pullTypes;
+
+  get typeNames => null;
+  get marketId => null;
+  get id => market.id;
+  get belongId => market.belongId;
+
+  constructor(XMarket store) {
+    market = store;
+    pullTypes = [TargetType.person, ...companyTypes];
+  }
+
+  @override
+  Future<bool> update(
+    String name,
+    String code,
+    String samrId,
+    String remark,
+    bool ispublic,
+    String photo,
+  ) async {
+    ResultType res = await kernel.updateMarket(MarketModel(
+        id: market.id,
+        name: name,
+        code: code,
+        samrId: samrId,
+        remark: remark,
+        photo: photo,
+        public: ispublic,
+        belongId: market.belongId));
+    if (res.success) {
+      XMarket xMarket = XMarket(
+        name: name,
+        code: code,
+        samrId: samrId,
+        remark: remark,
+        public: ispublic,
+        id: market.id,
+        photo: photo,
+        belongId: market.belongId,
+        createTime: '',
+        createUser: '',
+        status: 0,
+        updateTime: '',
+        updateUser: '',
+        version: '',
+        belong: null,
+        merchandises: [],
+        samr: null,
+        stags: [],
+        targetRelations: [],
+      );
+      market = xMarket;
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  Future<XMarketRelationArray?> getMember(PageRequest page) async {
+    return (await kernel.queryMarketMember({
+      id: market.id,
+      page: page,
+    } as IDBelongReq))
+        .data;
+  }
+
+  @override
+  Future<XMarketRelationArray?> getJoinApply(
+    PageRequest page,
+  ) async {
+    return (await kernel.queryJoinMarketApply({
+      id: market.id,
+      page: page,
+    } as IDBelongReq))
+        .data;
+  }
+
+  @override
+  Future<bool> approvalJoinApply(String id, int status) async {
+    return (await kernel.approvalJoinApply({id, status} as ApprovalModel))
+        .success;
+  }
+
+  @override
+  Future<bool> pullMember(
+      List<String> targetIds, List<String> typenames) async {
+    return (await kernel.pullAnyToMarket({
+      targetIds: targetIds,
+      marketId: market.id,
+      typeNames: pullTypes,
+    } as MarketPullModel))
+        .success;
+  }
+
+  @override
+  Future<bool> removeMember(List<String> targetIds) async {
+    return (await kernel.removeMarketMember({
+      targetIds: targetIds,
+      marketId: market.id,
+      typeNames: pullTypes,
+    } as MarketPullModel))
+        .success;
+  }
+
+  @override
+  Future<XMerchandiseArray?> getMerchandise(PageRequest page) async {
+    return (await kernel.searchMerchandise({
+      id: market.id,
+      page: page,
+    } as IDBelongReq))
+        .data;
+  }
+
+  @override
+  Future<XMerchandiseArray?> getMerchandiseApply(PageRequest page) async {
+    return (await kernel.queryMerchandiesApplyByManager({
+      id: market.id,
+      page: page,
+    } as IDBelongReq))
+        .data;
+  }
+
+  @override
+  Future<bool> approvalPublishApply(String id, int status) async {
+    return (await kernel.approvalMerchandise({id, status} as ApprovalModel))
+        .success;
+  }
+
+  @override
+  Future<bool> unPublish(String merchandiseId) async {
+    return (await kernel.deleteMerchandiseByManager({
+      id: merchandiseId,
+      belongId: market.belongId,
+    } as IDWithBelongReq))
+        .success;
+  }
+}
---
+++ lib/dart/core/market/resource.dart
@@ -0,0 +1,73 @@
+import "package:orginone/dart/base/api/kernelapi.dart";
+import "package:orginone/dart/base/model.dart";
+import "package:orginone/dart/base/schema.dart";
+import "package:orginone/dart/core/market/index.dart";
+
+/**
+ * 应用资源操作
+ */
+class Resource implements IResource {
+  final String teamId;
+  final List<String> destIds;
+  final String destType;
+  @override
+  final XResource resource;
+
+  Resource(this.resource,
+      {required this.teamId, required this.destIds, required this.destType});
+
+  @override
+  Future<bool> createExtend(
+    String teamId,
+    List<String> destIds,
+    String destType,
+  ) async {
+    ResultType resultType = await KernelApi.getInstance().createSourceExtend(
+        SourceExtendModel(
+            sourceId: resource.id,
+            sourceType: '资源',
+            destType: destType,
+            destIds: destIds,
+            teamId: teamId,
+            spaceId: resource.product!.belongId));
+
+    return resultType.success;
+  }
+
+  @override
+  Future<bool> deleteExtend(
+    String teamId,
+    List<String> destIds,
+    String destType,
+  ) async {
+    return (await kernel.deleteSourceExtend(SourceExtendModel(
+      sourceId: resource.id,
+      sourceType: '资源',
+      destIds: [],
+      destType: destType,
+      spaceId: resource.product!.belongId,
+      teamId: teamId,
+    )))
+        .success;
+  }
+
+  @override
+  Future<IdNameArray> queryExtend(
+    String destType,
+    String? teamId,
+  ) async {
+    return (await kernel.queryExtendBySource(SearchExtendReq(
+      sourceId: resource.id,
+      sourceType: '资源',
+      spaceId: resource.product!.belongId,
+      destType: '',
+      teamId: '',
+    )))
+        .data!;
+  }
+
+  @override
+  set resource(XResource _resource) {
+    // TODO: implement resource
+  }
+}
