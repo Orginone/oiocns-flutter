9445cbe36fb5ffb98581114ff7ce9c73e655b5d7
xy221150 <xy221150@gmail.com>
2023-04-07 17:45:13 +0800

    other:取消代码注释

--- lib/pages/login/register/logic.dart
+++ lib/pages/login/register/logic.dart
@@ -19,28 +19,28 @@ class RegisterController extends BaseController<RegisterState> {
   }
 
   void nextStep() {
-    // if (state.userNameController.text.isEmpty) {
-    //   ToastUtils.showMsg(msg: "请输入用户名");
-    //   return;
-    // }
-    // if (state.passWordController.text != state.verifyPassWordController.text) {
-    //   ToastUtils.showMsg(msg: "输入的两次密码不一致！");
-    //   return;
-    // }
-    // if (state.passWordController.text.length < 6) {
-    //   ToastUtils.showMsg(msg: "密码的长度不能小于6");
-    //   return;
-    // }
-    // if (state.passWordController.text.length > 15) {
-    //   ToastUtils.showMsg(msg: "密码的长度不能大于15");
-    //   return;
-    // }
-    // RegExp regExp = RegExp(r'(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{6,15}');
-    // Match? match = regExp.firstMatch(state.passWordController.text);
-    // if(match == null){
-    //  ToastUtils.showMsg(msg: '密码必须包含：数字、字母、特殊字符');
-    //  return;
-    // }
+    if (state.userNameController.text.isEmpty) {
+      ToastUtils.showMsg(msg: "请输入用户名");
+      return;
+    }
+    if (state.passWordController.text != state.verifyPassWordController.text) {
+      ToastUtils.showMsg(msg: "输入的两次密码不一致！");
+      return;
+    }
+    if (state.passWordController.text.length < 6) {
+      ToastUtils.showMsg(msg: "密码的长度不能小于6");
+      return;
+    }
+    if (state.passWordController.text.length > 15) {
+      ToastUtils.showMsg(msg: "密码的长度不能大于15");
+      return;
+    }
+    RegExp regExp = RegExp(r'(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{6,15}');
+    Match? match = regExp.firstMatch(state.passWordController.text);
+    if(match == null){
+     ToastUtils.showMsg(msg: '密码必须包含：数字、字母、特殊字符');
+     return;
+    }
 
     state.isStepOne.value = false;
   }
