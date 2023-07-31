ea93a8bc69b92de7e1aedcc219be59292dc0fdff
xy221150 <xy221150@gmail.com>
2023-07-31 17:13:17 +0800

    加入mock数据

--- lib/channel/wallet_channel.dart
+++ lib/channel/wallet_channel.dart
@@ -57,8 +57,44 @@ class WalletChannel {
         });
       }
       return records;
+    }else{
+      Map<String,dynamic> json = transactions;
+      if(json['result']!=null){
+        json['result'].forEach((json){
+          records.add(TransactionRecord.fromJson(json));
+        });
+      }
+      return records;
     }
     return records;
   }
 }
 
+
+const transactions = {
+  "id": 1,
+  "result": [{
+    "blocktime": 1605774673,
+    "fee": "0.007",
+    "from": "1P7P4v3kL39zugQgDDLRqxdddddddfKs",
+    "height": 11097152,
+    "note": "",
+    "status": "1",
+    "to": "1KgE3vayiqZKhfhMftN7vtddddMk941",
+    "txid": "0x3b4f885b01370509be01e258f2ddddd7fc01f65d578d9156a6887f3667c8d",
+    "type": "receive",
+    "value": "3"
+  }, {
+    "blocktime": 1602750506,
+    "fee": "0.007",
+    "from": "1KgE3vayiqZKhfhMftddddHoMk941",
+    "height": 10499717,
+    "note": "",
+    "status": "1",
+    "to": "1NkcL7c2LLESnZQgi2dddddKByemsPa5",
+    "txid": "0x4fc738785a0e792660858f65d5a9dddddccbc37b1229bd4c5d32fed1c7",
+    "type": "send",
+    "value": "0.1"
+  }],
+  "error": null
+};
