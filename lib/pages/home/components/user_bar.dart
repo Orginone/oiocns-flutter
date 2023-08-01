b6e1d3f49b7e11556c0ab0c22ff113b1c56c86c0
xy221150 <xy221150@gmail.com>
2023-08-01 17:05:08 +0800

    feat:头像菜单仿钉钉

--- lib/pages/home/components/user_bar.dart
+++ lib/pages/home/components/user_bar.dart
@@ -7,6 +7,7 @@ import 'package:orginone/dart/controller/user_controller.dart';
 import 'package:orginone/dart/core/chat/message/msgchat.dart';
 import 'package:orginone/dart/core/work/task.dart';
 import 'package:orginone/pages/store/state.dart';
+import 'package:orginone/routers.dart';
 import 'package:orginone/util/icons.dart';
 import 'package:orginone/widget/image_widget.dart';
 
@@ -34,52 +35,122 @@ class UserBar extends GetView<UserController> {
       child: Row(children: [
         Row(
           children: [
-            customPopupMenu(child: Padding(
-              padding: EdgeInsets.only(left: 10.w),
-              child: _imgAvatar(EdgeInsets.only(left: 10.w)),
-            ), children: SettingEnum.values
-                .map(
-                  (item) => GestureDetector(
-                behavior: HitTestBehavior.translucent,
-                onTap: () {
-                  controller.functionMenuController.hideMenu();
-                          controller.jumpSetting(item);
-                        },
-                child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          decoration: BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(
-                                  color: Colors.grey.shade300, width: 0.5),
-                            ),
-                          ),
-                          child: Row(
-                            children: <Widget>[
-                              Icon(
-                                item.icon,
-                                size: 24.w,
-                                color: Colors.black,
+            customPopupMenu(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10.w),
+                  child: _imgAvatar(EdgeInsets.only(left: 10.w)),
+                ),
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      controller.functionMenuController.hideMenu();
+                      Get.toNamed(Routers.userInfo);
+                    },
+                    child: Row(
+                      children: [
+                        _imgAvatar(
+                            EdgeInsets.symmetric(
+                                vertical: 5.h, horizontal: 10.w),
+                            size: 60,
+                            circular: false,
+                            radius: 8.w),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox(
+                              width: 240.w,
+                              child: Row(
+                                children: [
+                                  Text(controller.user.metadata.name ?? ""),
+                                  Expanded(
+                                    child: Container(
+                                      margin: EdgeInsets.only(right: 10.w),
+                                      alignment: Alignment.centerRight,
+                                      child: IconButton(
+                                        onPressed: () {
+                                          controller.functionMenuController.hideMenu();
+                                          Get.toNamed(
+                                            Routers.shareQrCode,
+                                            arguments: {
+                                              "entity": controller.user.metadata
+                                            },
+                                          );
+                                        },
+                                        icon: Icon(
+                                          Ionicons.qr_code_outline,
+                                          size: 24.w,
+                                        ),
+                                        padding: EdgeInsets.zero,
+                                        constraints:
+                                            BoxConstraints(maxHeight: 24.w),
+                                      ),
+                                    ),
+                                  )
+                                ],
                               ),
-                              Expanded(
-                                child: Container(
-                                  margin: const EdgeInsets.only(left: 10),
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 8),
-                                  child: Text(
-                                    item.label,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 24.sp,
+                            ),
+                            SizedBox(
+                              height: 10.h,
+                            ),
+                            SizedBox(
+                              child: Text(
+                                controller.user.metadata.remark ?? "",
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                                style: TextStyle(fontSize: 16.sp),
+                              ),
+                              width: 240.w,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  ...SettingEnum.values
+                      .map(
+                        (item) => GestureDetector(
+                          behavior: HitTestBehavior.translucent,
+                          onTap: () {
+                            controller.functionMenuController.hideMenu();
+                            controller.jumpSetting(item);
+                          },
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 10.w),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                    color: Colors.grey.shade300, width: 0.5),
+                              ),
+                            ),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  item.icon,
+                                  size: 24.w,
+                                  color: Colors.black,
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    margin: EdgeInsets.only(left: 10.w),
+                                    padding:
+                                        EdgeInsets.symmetric(vertical: 15.h),
+                                    child: Text(
+                                      item.label,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 24.sp,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                    .toList(),
+                      )
+                      .toList()
+                ],
                 controller: controller.functionMenuController),
             SizedBox(
               width: 10.w,
@@ -200,7 +271,11 @@ class UserBar extends GetView<UserController> {
     );
   }
 
-  Widget _imgAvatar(EdgeInsets insets) {
+  Widget _imgAvatar(EdgeInsets insets,
+      {BoxFit fit = BoxFit.cover,
+      bool circular = true,
+      double size = 45,
+      double? radius}) {
     return Obx(() {
       dynamic avatar;
       var share = controller.provider.user?.share;
@@ -211,9 +286,10 @@ class UserBar extends GetView<UserController> {
           margin: insets,
           child: ImageWidget(
             avatar,
-            fit: BoxFit.cover,
-            size: 45.w,
-            circular: true,
+            fit: fit,
+            size: size.w,
+            circular: circular,
+            radius: radius,
           ));
     });
   }
